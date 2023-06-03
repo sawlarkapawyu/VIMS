ad45b93d74588e8e66cba8716ce46b45cc75b887
st122632 <109932738+st122632@users.noreply.github.com>
2023-06-03 23:54:45 +0700

    Edit Dev1

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-/** @type {import('next').NextConfig} */
-
 const { i18n } = require("./next-i18next.config");
 
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
-  // experimental: {
-  //   scrollRestoration: true,
-  // },
-}
-
-// module.exports = nextConfig
-module.exports = {
-  // pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  i18n: {
-    locales: ['default', 'en', 'mm'],
-    defaultLocale: 'default',
-  },
 };
